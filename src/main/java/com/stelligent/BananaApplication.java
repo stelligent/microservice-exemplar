8f9c9feb257a4f6e41b1c86df5172dec40344da1
Casey Lee <cplee@nektos.com>
2016-09-12 11:52:11 -0700

    init

---
+++ src/main/java/com/stelligent/BananaApplication.java
@@ -0,0 +1,12 @@
+package com.stelligent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BananaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BananaApplication.class, args);
+	}
+}
