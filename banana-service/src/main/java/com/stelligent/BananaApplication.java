d0bdbf7e376a448cc982db92fe686e7b21206cd7
Casey Lee <cplee@nektos.com>
2016-09-27 22:21:25 -0700

    remove cruft

--- banana-service/src/main/java/com/stelligent/BananaApplication.java
+++ banana-service/src/main/java/com/stelligent/BananaApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan
 @SpringBootApplication
 public class BananaApplication {
 
